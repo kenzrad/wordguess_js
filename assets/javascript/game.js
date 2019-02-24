28541e9bba89d43fd353e9664ed1c443029d7da2
barkgnar <45748185+kenzrad@users.noreply.github.com>
2019-02-24 11:22:35 -0500

    pseudocodin

---
+++ assets/javascript/game.js
@@ -0,0 +1,3 @@
+//use keytouch event (any key) to initiate word selection
+//create array for word choices
+var videoGames = ["New Vegas", "Hyrule", "Link", "Navi says Listen", "Whitetail", "The Horde", "The Elder Scrolls", "Bethesda Studios", "Greybeards", "Sovngarde", "Capital Wasteland", "Nuclear Fallout", "Fat Man", "Combat Shotgun", "SPECIAL", "Triforce", "Ocarina of Time", "Epona", "Master Sword",  "Gananondorf", "Great Deku Tree"]
