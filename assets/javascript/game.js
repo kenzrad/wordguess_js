e6ea4d59dec853742b60e90e25a227ad445c7bed
barkgnar <45748185+kenzrad@users.noreply.github.com>
2019-02-25 08:30:11 -0500

    ITS ALIIIIIIIVE

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,63 @@
 //use keytouch event (any key) to initiate word selection
 //create array for word choices
-var videoGames = ["New Vegas", "Hyrule", "Link", "Navi says Listen", "Whitetail", "The Horde", "The Elder Scrolls", "Bethesda Studios", "Greybeards", "Sovngarde", "Capital Wasteland", "Nuclear Fallout", "Fat Man", "Combat Shotgun", "SPECIAL", "Triforce", "Ocarina of Time", "Epona", "Master Sword",  "Gananondorf", "Great Deku Tree"]
+var words = [
+    "New Vegas", 
+    "Hyrule", 
+    "Link", 
+    "Navi", 
+    "Whiterun", 
+    "Horde",
+    "Magicka", 
+    "Bethesda", 
+    "Greybeards", 
+    "Sovngarde", 
+    "Wasteland", 
+    "SPECIAL", 
+    "Triforce", 
+    "Ocarina", 
+    "Epona", 
+    "Gananondorf",
+    "Deku"
+];
+
+//pick a random word from the array
+var word = words[Math.floor(Math.random() * words.length)];
+// console.log(word);
+
+//Get the player's guess
+// If the word has not been guessed {
+//     Show the player current progress
+// }
+
+//log the player's guess
+
+//check if the guess is valid
+// If the player guess matches any letter of the random word {
+//     Reveal that letter in the word AND alert(letters log)
+// }
+// Else the player guesses a letter that doesn't match {
+//     Log the guess and AND alert(letters log) AND alert(guesses log)
+// }
+
+//Log wins
+
+//Log losses
+//If the letter
+
+
+//Current progress:
+//Letters guessed
+//Remaining guesses
+//
+//END GAME OPTIONS
+// If the player want to quit game {
+//     Quit Game
+// }
+
+// If the player guesses the word {
+//     End game and log win
+// }
+
+// If the player does not guess the word AND choices are up {
+//     End game and log loss
+// }
